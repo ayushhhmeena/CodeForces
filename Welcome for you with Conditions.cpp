5d91b2e98f92422e4e235565813dc235b9dd7147
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2023-12-10 21:48:04 +0530

    Create Welcome for you with Conditions.cpp

---
+++ Welcome for you with Conditions.cpp
@@ -0,0 +1,16 @@
+/* 
+Author:- Ayush Meena
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    unsigned int a,b; cin>>a>>b;
+    if(a>b || a==b){
+        cout<<"Yes";
+    }else{
+        cout<<"No";
+    }
+    return 0;
+}
