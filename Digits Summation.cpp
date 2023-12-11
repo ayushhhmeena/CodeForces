1e4fabf3004f96cee8369b9ea70a659e7431ca3a
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2023-12-11 13:40:43 +0530

    Create Digits Summation.cpp

---
+++ Digits Summation.cpp
@@ -0,0 +1,13 @@
+/* 
+Author:- Ayush Meena
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    ll a,b; cin>>a>>b;
+    cout<<(a%10)+(b%10);
+    return 0;
+}
