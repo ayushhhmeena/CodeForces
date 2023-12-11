35eda5afac57e3cb9d5e150ff94700c3572ffaf5
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2023-12-11 11:05:44 +0530

    Create  The Brothers.cpp

---
+++ The Brothers.cpp
@@ -0,0 +1,17 @@
+/* 
+Author:- Ayush Meena
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    string a,b,c,d; cin>>a>>b>>c>>d;
+    if(b==d){
+        cout<<"ARE Brothers";
+    }else{
+        cout<<"NOT";
+    }
+    return 0;
+}
