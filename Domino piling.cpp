e26cc0ab45988915e873c6465b29e3c68c96ecda
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2024-01-07 22:32:58 +0530

    Create Domino piling.cpp

---
+++ Domino piling.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void soln(){
+    ll m,n; cin>>m>>n;
+    cout<<(m*n)/2;
+}
+int main(){
+    ll t=1; 
+    //cin>>t;
+    while(t--){
+        soln();
+    }
+    return 0;
+}
