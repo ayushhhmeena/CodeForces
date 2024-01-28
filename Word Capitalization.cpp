e19225b46705bc4d768cafd7881bf35e200abbf1
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2024-01-28 23:15:12 +0530

    Create Word Capitalization.cpp

---
+++ Word Capitalization.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void soln(){
+    string s; cin>>s;
+    s[0]=toupper(s[0]);
+    cout<<s;
+}
+int main(){
+    ll t=1; 
+    //cin>>t;
+    while(t--){
+        soln();
+    }
+    return 0;
+}
