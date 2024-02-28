9842e1d53623b66b602c3f996132cec1b5c1a488
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2024-02-29 01:41:50 +0530

    Add files via upload

---
+++ A_Again_Twenty_Five.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void sol(){
+    ll n; cin>>n;
+    cout<<pow(5,n);
+}
+int main(){
+    ll t=1;
+    //cin>>t;
+    while(t--){
+        sol();
+    }
+    return 0;
+}
